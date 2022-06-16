6da2404b30ca057066263718c9757f7dfa67b5e2
James McLeod <james.mcleod@finos.org>
2022-06-16 08:33:34 +0100

    update baseURL for subdomain registration
    Signed-off-by: James McLeod <james.mcleod@finos.org>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,7 +12,7 @@ module.exports = {
   title: `${projectName}`,
   tagline: `${projectName}`,
   url: 'https://linuxfoundation.github.io/',
-  baseUrl: '/lf-podcast/',
+  baseUrl: '/',
   favicon: 'img/favicon/cropped-favicon-80x80.png',
   projectName: 'lf-podcast',
   organizationName: 'linuxfoundation',

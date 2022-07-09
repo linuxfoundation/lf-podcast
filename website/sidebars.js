cb77e2655c85aecff50d7322cb499baa99e9fced
James McLeod <james.mcleod@finos.org>
2022-07-09 11:47:32 +0100

    update navigation config
    Signed-off-by: James McLeod <james.mcleod@finos.org>

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   mainSidebar: {
-    "Podcasts" : ["podcasts/management-and-diversity", "podcasts/new-model-training", "podcasts/business-of-open-source", "podcasts/openssf-project", "podcasts/priyanka-sharma-gm-cncf", "podcasts/introduction-to-lf-podcast"],
+    "Podcasts" : ["podcasts/building-open-source-strategy", "podcasts/management-and-diversity", "podcasts/new-model-training", "podcasts/business-of-open-source", "podcasts/openssf-project", "podcasts/priyanka-sharma-gm-cncf", "podcasts/introduction-to-lf-podcast"],
     "About LF Podcast": ["podcasts-intro", "team"],
     "Adding New Podcasts": ["templates/podcast-template"]
   },

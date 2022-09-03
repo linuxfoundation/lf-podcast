0f62394b50795b588f7a53a75c5311c68e70db75
James McLeod <james.mcleod@finos.org>
2022-09-03 21:10:34 +0100

    config nav and menu
    Signed-off-by: James McLeod <james.mcleod@finos.org>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       items: [
         {to: 'docs/podcasts-intro', label: 'Introduction', position: 'right'},
-        {to: 'docs/podcasts/lfnetworking-edge-computing', label: 'Podcasts', position: 'right'},
+        {to: 'docs/podcasts/waiting-for-sbom', label: 'Podcasts', position: 'right'},
         {to: 'docs/templates/podcast-template', label: 'Add New Podcast', position: 'right'},
         {href: 'https://feeds.captivate.fm/untold-stories-of-open-source/', label: 'RSS', position: 'right'},        
         {href: 'https://github.com/linuxfoundation/lf-podcast', label: 'GitHub', position: 'right'}
